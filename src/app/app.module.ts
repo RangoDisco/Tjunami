9eadfc6182a5b30da6bd4cd7bf4817ba109d5108
Mauranne Lagneau <mauranne.lagneau@gmail.com>
2021-05-11 14:20:12 +0200

    adding page 'Movies'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesPageComponent } from './movies-page/movies-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MoviesPageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
