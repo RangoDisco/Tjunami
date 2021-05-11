adffbd4de2b4ede0475f636ab99ddb26489880eb
Mauranne Lagneau <mauranne.lagneau@gmail.com>
2021-05-11 15:48:26 +0200

    adding overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesPageComponent } from './movies-page/movies-page.component';
+import { TruncatorPipe } from './common/truncator.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MoviesPageComponent],
+  declarations: [AppComponent, MoviesPageComponent, TruncatorPipe],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
