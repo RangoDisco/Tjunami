9e9846fc8badb3cfa598ac87c5bced36e82e7603
RangoDisco <maxime-dias_student2021@wilder.school>
2021-05-12 16:23:56 +0200

    deploy

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 app.use(express.static('./dist/tjunami'));
 
-app.get('/', (req, res) =>
-    res.sendFile('index.html', { root: "dist/tjunami" }),
-);
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/tjunami' }
+  );
+});
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
